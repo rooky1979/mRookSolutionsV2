45c5dfd214522656581f27093fd66fd871e30977
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-11-01 18:50:56 +1300

    initial setup

---
+++ app/components/Introduction.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "../styles/IntroductionHome.module.css";
+import { motion } from "framer-motion";
+
+const Introduction = () => {
+
+/*   const logoVariants = {
+    hidden: {
+      x: -100,
+      opacity: 0,
+    },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        duration: 1, // Adjust the duration as needed
+      },
+    },
+  }; */
+
+  return (
+    <div className={styles.introcontainer}>
+      <div className={styles.logo}>
+        <img src="/mainlogo.png" alt="Rook bird on a Rook Chesspiece"/>
+      </div>
+      <div className={styles.paragraph}>
+        <p className={styles.intro1}>
+          A Journey of a thousand miles
+        </p>
+        <p className={styles.intro2}>begins with a single step...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
