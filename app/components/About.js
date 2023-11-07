d0a035e6501c8e468ec361da796efc143b6fef0a
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2023-11-08 11:30:24 +1300

    Adding animations

--- app/components/About.js
+++ app/components/About.js
@@ -1,10 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from '../styles/AboutHomepage.module.css';
 import Timeline from './Timeline';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
   const [scrollY, setScrollY] = useState(0);
+  const aboutTitleRef = useRef(null);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,22 +22,30 @@ const About = () => {
     };
   }, []);
 
-  const animationControls = {
-    initial: { opacity: 0, y: -100 },
-    visible: { opacity: 1, y: 0 },
-  };
+  useEffect(() => {
+    if (aboutTitleRef.current) {
+      gsap.to(aboutTitleRef.current, {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: aboutTitleRef.current,
+          start: 'top 80%',
+          end: 'top 60%',
+          toggleActions: 'play none none reverse',
+        },
+      });
+    }
+  }, [aboutTitleRef]);
 
   return (
     <section className={styles.aboutsection}>
-      <motion.h2
-      className={styles.aboutTitle}
-        initial={animationControls.initial}
-        animate={scrollY > 100 ? 'visible' : 'initial'}
-        variants={animationControls}
-        transition={{ type: 'spring', damping: 25 }}
+      <h2
+        ref={aboutTitleRef}
+        className={styles.aboutTitle}
+        style={{ opacity: 0 }}
       >
         About...
-      </motion.h2>
+      </h2>
       <Timeline scrollY={scrollY}>
         {/* Your timeline elements */}
       </Timeline>
