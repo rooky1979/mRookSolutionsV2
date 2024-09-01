b15167030fc77415f2b4fd98a8e9d737f0e28182
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2024-09-02 11:06:53 +1200

    Fixes for deployment

--- app/about/page.js
+++ app/about/page.js
@@ -1,11 +1,11 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import aboutStyles from "../styles/About.module.css";
 import { motion } from "framer-motion";
 import PhotoModel from "../components/PhotoModel";
 
-export default function about() {
+export default function About() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentPhoto, setCurrentPhoto] = useState(null);
 
@@ -37,7 +37,11 @@ export default function about() {
           animate={{ opacity: 1 }}
           transition={{ duration: 1.5, delay: 0.6 }}
         >
-          <img src="/me.jpeg" alt="Me at Stonehenge" onClick={() => openModel("/me.jpeg")} />
+          <img
+            src="/me.jpeg"
+            alt="Me at Stonehenge"
+            onClick={() => openModel("/me.jpeg")}
+          />
         </motion.div>
         <motion.div
           className={aboutStyles.textcontainer}
@@ -55,24 +59,26 @@ export default function about() {
           </p>
           <br />
           <p className={aboutStyles.text}>
-            In 2014 during a big project of installing, upgrading, and qualifying
-            HPLCs, and their PCs I wished I had gone into IT instead of science,
-            but telling myself not to do anything about it with things like "I'm
-            too old", or "I can't afford to go to University, I have a mortgage
-            to pay". I recognise now exactly those "justifications" for what
-            they were..... Excuses and Fear.
+            In 2014 during a big project of installing, upgrading, and
+            qualifying HPLCs, and their PCs I wished I had gone into IT instead
+            of science, but telling myself not to do anything about it with
+            things like &quot;I&apos;m too old&quot;, or &quot;I can&apos;t
+            afford to go to University, I have a mortgage to pay&quot;. I
+            recognise now exactly those &quot;justifications&quot; for what they
+            were..... Excuses and Fear.
           </p>
           <br />
           <p className={aboutStyles.text}>
-            In 2017 after some self-reflection came to pass, I decided to go
-            for it. No more self-justifications, rationalisations, and excuses.
-            I enrolled at AUT full-time in a BSc Computer and Information
-            Sciences degree majoring in Software Development, and I have never
-            looked back. To switch careers at 39 was one of the scariest things
-            I have ever done. The fear of the unknown was intense. Self doubt
-            and imposter syndrome were rampant. "What if I fail?" But I seem to
-            operate under pressure best and when I want something, I go for it.
-            Hey, if I'm going to walk on thin ice, I may as well tap dance right?
+            In 2017 after some self-reflection came to pass, I decided to go for
+            it. No more self-justifications, rationalisations, and excuses. I
+            enrolled at AUT full-time in a BSc Computer and Information Sciences
+            degree majoring in Software Development, and I have never looked
+            back. To switch careers at 39 was one of the scariest things I have
+            ever done. The fear of the unknown was intense. Self-doubt and
+            imposter syndrome were rampant. &quot;What if I fail?&quot; But I
+            seem to operate under pressure best and when I want something, I go
+            for it. Hey, if I&apos;m going to walk on thin ice, I may as well
+            tap dance, right?
           </p>
           <br />
           <p className={aboutStyles.text}>
@@ -81,7 +87,8 @@ export default function about() {
             the learning track as a Consultant/Specialist in SAP ABAP
             development and on course to become a full-stack developer (SAP
             Fiori, CAP, RAP, CPI, etc.) where I currently reside and building
-            little personal projects in my own time to keep learning new skills and stay relevant.
+            little personal projects in my own time to keep learning new skills
+            and stay relevant.
           </p>
           <br />
           <p className={aboutStyles.text}>
@@ -89,8 +96,8 @@ export default function about() {
             have from my previous career is a wealth of transferable skills and
             life experience. I came out of university to my job knowing that in
             order to progress in this new career, I must stay humble. Without
-            ego. So that I can listen and learn. To be open minded to new ideas
-            so that I remain teachable so I can continue to grow. My fears
+            ego. So that I can listen and learn. To be open-minded to new ideas
+            so that I remain teachable, so I can continue to grow. My fears
             turned out to be some of my greatest assets.
           </p>
           <br />
@@ -98,20 +105,21 @@ export default function about() {
             I live my life by some simple values that I believe are paramount in
             both professional and personal life. Honesty, loyalty, integrity,
             accountability, and being of service. Living by these values earns
-            trust which is the foundation of any relationship both business and
-            personal.
+            trust, which is the foundation of any relationship, both business
+            and personal.
           </p>
           <br />
           <p className={aboutStyles.text}>
-            I laugh, I joke. I stay child-like with curiousity and wonder, but not childish. I surf, I
-            skate, I play pinball and retro games. I love cinema and spending
-            time with family and friends. I love life.
+            I laugh, I joke. I stay child-like with curiosity and wonder, but
+            not childish. I surf, I skate, I play pinball and retro games. I
+            love cinema and spending time with family and friends. I love life.
           </p>
           <br />
           <p className={aboutStyles.text}>
-            But don't let my cheerful demeanour fool you. When it's time to get
-            down to business, I am a man of sheer will and determination. I get
-            things done. I say what I'll do and do what I say.
+            But don&apos;t let my cheerful demeanour fool you. When it&apos;s
+            time to get down to business, I am a man of sheer will and
+            determination. I get things done. I say what I&apos;ll do and do
+            what I say.
           </p>
         </motion.div>
         <motion.div
@@ -120,11 +128,15 @@ export default function about() {
           animate={{ opacity: 1 }}
           transition={{ duration: 1.5, delay: 0.6 }}
         >
-          <img src="/family.jpeg" alt="Family at Disney" onClick={() => openModel("/family.jpeg")}/>
+          <img
+            src="/family.jpeg"
+            alt="Family at Disney"
+            onClick={() => openModel("/family.jpeg")}
+          />
         </motion.div>
         {isModalOpen && (
-        <PhotoModel imageUrl={currentPhoto} onClose={closeModel} />
-      )}
+          <PhotoModel imageUrl={currentPhoto} onClose={closeModel} />
+        )}
       </div>
     </>
   );
